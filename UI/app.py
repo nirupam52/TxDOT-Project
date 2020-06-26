8b96ba465ce0a93b4057fa37f7cd43d203d59fdc
Nirupam Bidikar <nirupam52@gmail.com>
2020-06-25 19:17:18 -0500

    transition to using ui files to build interface

--- UI/app.py
+++ UI/app.py
@@ -1,45 +1,18 @@
 import sys
+sys.path.insert(1,'../')
 import os
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5 import QtWidgets, uic
+from designer_files.mainWindow import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
+        self.setupUi(self)
         self.setWindowTitle("TxDOT ISPE Tool")
         self.setFixedSize(600,600)
-    #the 4 buttons 
-        buttonLayout = QHBoxLayout()
-        btn1 = QPushButton("OP1")
-        btn2 = QPushButton("OP2")
-        btn3 = QPushButton("OP3")
-        btn4 = QPushButton("OP4")
-        buttonLayout.addWidget(btn1)
-        buttonLayout.addWidget(btn2)
-        buttonLayout.addWidget(btn3)
-        buttonLayout.addWidget(btn4)
-        buttonWidget = QWidget()
-        buttonWidget.setLayout(buttonLayout)
-        
-    #button layout and image on top
-        mainLayout = QGridLayout()
-        logo = QLabel()
-        imgPath = os.path.abspath(os.getcwd())
-        pixmap = QPixmap(imgPath + '/assets/txdot_logo.jpg')
-        logo.setPixmap(pixmap)
-        mainLayout.addWidget(logo,0,0)
-        mainLayout.addWidget(buttonWidget,1,1)
-        mainWidget = QWidget()
-        mainWidget.setLayout(mainLayout)
-        self.setCentralWidget(mainWidget)
 
-
-        
-
-app = QApplication(sys.argv)
-
+app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
 window.show()
 
